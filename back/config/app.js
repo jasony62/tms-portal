32882dc2cdd81412102d806dcd44e9f2adcdc403
b-ss <2764411477@qq.com>
2020-06-20 01:15:53 +0800

    modify some var

--- back/config/app.js
+++ back/config/app.js
@@ -1,22 +1,22 @@
 module.exports = {
-  port: process.env.TMS_APP_PORT || 3000,
-  name: process.env.TMS_APP_NAME || 'tms-portal-back',
-  router: {
-    auth: {
-      prefix: 'auth', // 接口调用url的前缀
-    },
-    controllers: {
-      prefix: 'api', // 接口调用url的前缀，例如：/api
-    },
-  },
-  auth: {
-    disabled: true,
-    captcha: { code: 'a1z9' },
-    client: { accounts: [{ id: 1, username: 'user1', password: '123456' }] },
-    jwt: {
-      privateKey: 'tms-back-secret',
-      expiresIn: 7200,
-    },
-  },
-  tmsTransaction: false,
+	port: process.env.TMS_APP_PORT || 3000,
+	name: process.env.TMS_APP_NAME || 'tms-portal-back',
+	router: {
+		auth: {
+			prefix: 'auth', // 接口调用url的前缀
+		},
+		controllers: {
+			prefix: 'api', // 接口调用url的前缀，例如：/api
+		},
+	},
+	auth: {
+		disabled: true,
+		captcha: { code: 'a1z9' },
+		client: { accounts: [{ id: 1, username: 'user1', password: '123456' }] },
+		jwt: {
+			privateKey: 'tms-back-secret',
+			expiresIn: 7200,
+		},
+	},
+	tmsTransaction: false,
 }
--- back/ecosystem.config.js
+++ back/ecosystem.config.js
@@ -1,19 +1,26 @@
 module.exports = {
-  apps: [
-    {
-      name: 'tms-portal-back',
-      script: 'node server',
+	apps: [
+		{
+			name: 'tms-portal-back',
+			script: 'node server',
 
-      instances: 1,
-      autorestart: true,
-      watch: true,
-      ignore_watch: ['node_modules', 'tests', 'files'],
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-        TMS_PORTAL_MONGODB_HOST: 'localhost',
-        TMS_PORTAL_MONGODB_PORT: 27017
-      }
-    }
-  ]
+			instances: 1,
+			autorestart: true,
+			watch: true,
+			ignore_watch: ['node_modules', 'tests', 'files'],
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'development',
+				TMS_PORTAL_MONGODB_HOST: 'localhost',
+				TMS_PORTAL_MONGODB_PORT: 27017
+			},
+			env_tenant: {
+				NODE_ENV: 'tenant',
+				TMW_REQUIRE_BUCKET: 'yes',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
+		}
+	]
 }
