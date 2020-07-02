2e2ba5cef81d52a48b4534bf12790f6c88d2dbd4
b-ss <2764411477@qq.com>
2020-07-02 10:58:35 +0800

    remove more code

--- ue_portal/src/components/CompRoute.vue
+++ ue_portal/src/components/CompRoute.vue
@@ -11,7 +11,6 @@
       <div>$route.meta: {{ $route.meta }}</div>
     </div>
     <hr />
-    <!-- <comp-online ref="onlineComp" :url="lib.url" :includeCss="lib.includeCss" :props="onlineProps" :events="onlineEvents"></comp-online> -->
     <div class="box">
       <comp-online ref="onlineComp" :url="lib.url" :includeCss="lib.includeCss" :props="onlineProps" :events="onlineEvents"></comp-online>
     </div>
