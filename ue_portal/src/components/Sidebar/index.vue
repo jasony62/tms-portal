d981c31bff051736b155311c2df2ddff0947e05b
b-ss <2764411477@qq.com>
2020-10-23 09:33:46 +0800

    1015添加退出登录

--- ue_portal/src/components/Sidebar/index.vue
+++ ue_portal/src/components/Sidebar/index.vue
@@ -3,6 +3,7 @@
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <el-menu :default-active="activeMenu" :collapse="isCollapse" :background-color="variables.menuBg" :text-color="variables.menuText" :unique-opened="false" :active-text-color="variables.menuActiveText" :collapse-transition="false" mode="vertical">
         <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+        <el-menu-item index="logout" :text-color="variables.menuText" @click="logout()">退出登录</el-menu-item>
       </el-menu>
     </el-scrollbar>
   </div>
@@ -37,6 +38,12 @@ export default {
     this.$router.addRoutes(dynaRoutes)    
     if (this.$route.path === '/') {
       this.$router.push({ name: dynaRoutes[0].name })
+    }
+  },
+  methods: {
+    logout() {
+      sessionStorage.removeItem('access_token')
+      window.location.reload()      
     }
   }
 }
