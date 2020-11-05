38ac56abda88cc8b9ac0756586abfae1b99a7e7a
b-ss <2764411477@qq.com>
2020-11-05 08:47:34 +0800

    1112查看号码

--- ue_portal/src/components/Sidebar/index.vue
+++ ue_portal/src/components/Sidebar/index.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
-    <el-scrollbar wrap-class="scrollbar-wrapper">
-      <el-menu :default-active="activeMenu" :collapse="isCollapse" :background-color="variables.menuBg" :text-color="variables.menuText" :unique-opened="false" :active-text-color="variables.menuActiveText" :collapse-transition="false" mode="vertical">
-        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
-        <el-menu-item index="logout" :text-color="variables.menuText" @click="logout()">退出登录</el-menu-item>
+    <div class="sidebar-wrapper">
+      <el-menu
+        class="sidebar-el-menu"
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item
+          v-for="route in routes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        />
+        <li style="flex:1;"></li>
+        <el-menu-item
+          class="logout"
+          index="logout"
+          :text-color="variables.menuText"
+          @click="logout()"
+          >退出登录</el-menu-item
+        >
       </el-menu>
-    </el-scrollbar>
+    </div>
   </div>
 </template>
 <script>
@@ -35,7 +57,7 @@ export default {
   },
   async created() {
     let dynaRoutes = await this.$store.dispatch('router/generateRoutes')
-    this.$router.addRoutes(dynaRoutes)    
+    this.$router.addRoutes(dynaRoutes)
     if (this.$route.path === '/') {
       this.$router.push({ name: dynaRoutes[0].name })
     }
@@ -43,8 +65,38 @@ export default {
   methods: {
     logout() {
       sessionStorage.removeItem('access_token')
-      window.location.reload()      
+      window.location.reload()
     }
   }
 }
 </script>
+<style lang="scss">
+.sidebar-wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+  .sidebar-el-menu:not(.el-menu--collapse) {
+    width: 240px;
+    display: flex;
+    flex-direction: column;
+  }
+  > ul {
+    height: 100%;
+    .logout {
+      flex: 0 0 56px;
+      box-sizing: border-box;
+      display: block;
+      width: 100%;
+      height: 100%;
+      align-items: center;
+      font-size: 14px;
+      color: #fff;
+    }
+  }
+}
+</style>
